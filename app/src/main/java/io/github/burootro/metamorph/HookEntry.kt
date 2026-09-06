25e7fd39225332148618ce015d657ae64a573574
burootro <businessroot440@outlook.com>
2026-09-06 06:27:38 +0300

    Update HookEntry.kt

--- app/src/main/java/io/github/burootro/metamorph/HookEntry.kt
+++ app/src/main/java/io/github/burootro/metamorph/HookEntry.kt
@@ -6,7 +6,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 import io.github.burootro.metamorph.core.Feature
 import io.github.burootro.metamorph.core.Prefs
 import io.github.burootro.metamorph.features.TestHook
-import io.github.burootro.metamorph.features.VideoProbe
+import io.github.burootro.metamorph.features.VideoDownload
 
 class HookEntry : IXposedHookLoadPackage {
 
@@ -25,7 +25,7 @@ class HookEntry : IXposedHookLoadPackage {
         Feature.runAll(
             listOf(
                 TestHook(cl, prefs),
-                VideoProbe(cl, prefs)
+                VideoDownload(cl, prefs)
             )
         )
     }
